cc3910a54846c81722e2500401b9ac6ead3a6e01
= <=>
2022-05-18 15:58:41 -0500

    It has been initialized the project, with his folders and dependencies basic

---
+++ src/main/java/com/DeliElegidos/rentalCarProject/RentalCarProjectApplication.java
@@ -0,0 +1,13 @@
+package com.DeliElegidos.rentalCarProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalCarProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DeliElegidos/rentalCarProject/RentalCarProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DeliElegidos.rentalCarProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
